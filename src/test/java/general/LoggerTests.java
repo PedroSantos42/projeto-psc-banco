4c15882abd8f468166ffb01414e4bf1e86e5962d
Pedro Santos <pedro.barros.santos@avanade.com>
2019-10-11 10:16:29 -0300

    Logger

--- src/test/java/general/LoggerTests.java
+++ src/test/java/general/LoggerTests.java
@@ -1,19 +1,31 @@
 package general;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import util.MyLogger;
+import util.MyStrings;
 
 public class LoggerTests {
 
-	private MyLogger _logger;
+	private static MyLogger _logger;
+	private Stream<String> stream;
 	
 	@BeforeClass
 	public static void setUp() {
 		System.out.println("Iniciando Logger testes...");
-		MyLogger _logger = new MyLogger();
+		_logger = new MyLogger();
+		_logger.getInstance(MyStrings.LOGGER_SOURCE_FILE);
 	}
 	
 	@AfterClass
@@ -22,17 +34,20 @@ public class LoggerTests {
 	}
 	
 	@Test
-	public void log_ValidErrorMessage_WriteToLoggerFile() {
+	public void log_ValidErrorMessage_WriteToLoggerFile() throws IOException {
 		// arrange
 		String message = "Mensagem de erro teste";
 		
 		// act
+		_logger.log(message);
+		
+		StringBuilder contentBuilder = new StringBuilder();
+		
+		stream = Files.lines(Paths.get(MyStrings.LOGGER_SOURCE_FILE), StandardCharsets.UTF_8);
+		
+		stream.forEach(s -> contentBuilder.append(s));
 		
 		// assert
-	}
-	
-	@Test
-	public void formatDate_ValidDate_ReturnDDMMAAAAFormat() {
-		
+		assertEquals(message, contentBuilder.toString().substring(contentBuilder.toString().indexOf(message.charAt(0))));
 	}
 }
