e96278e7b79de0286e002d5c33082c7d2f3e8593
João Gilberto <joaogilberto_ns@yahoo.com.br>
2019-10-21 15:55:08 -0300

    Implementação de mais testes nos DAOs de contas.

--- src/test/java/dao/ContaCorrenteDAOTests.java
+++ src/test/java/dao/ContaCorrenteDAOTests.java
@@ -1,6 +1,11 @@
 package dao;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -11,46 +16,45 @@ import dao.pessoa.ClienteDAO;
 import org.junit.Ignore;
 import model.conta.ContaCorrente;
 import model.pessoa.Pessoa;
-import util.UtilJPA;
 
 public class ContaCorrenteDAOTests {
 
 	private ContaCorrenteDAO _dao = new ContaCorrenteDAO();
-	
+
 	@BeforeClass
 	public static void setUp() {
 		System.out.println("Starting Conta DAO Tests...");
 	}
-	
+
 	@AfterClass
 	public static void tearDown() {
 		System.out.println("Finishing Conta DAO Tests...");
 //		UtilJPA.closeEntityManagerFactory();
 	}
-	
+
 	@Test
 	public void add_ValidArg_AddToDatabase() {
 		// arrange
-		int id = 1; 
+		int id = 1;
 		int numeroConta = 1;
 		double saldo = 100.0;
 		String situacao = "a";
-		
+
 		Pessoa p = new Pessoa();
 		p.setId(1);
 		p = (Pessoa) new ClienteDAO().listByPrimaryKey(p.getClass(), p.getPrimaryKey());
-		
-		ContaCorrente conta = new ContaCorrente(id, numeroConta, saldo, situacao, p, null);
-		
+
+		ContaCorrente contaCorrente = new ContaCorrente(id, numeroConta, saldo, situacao, p, null);
+
 		// act
-		_dao.add(conta);
-		
-		ContaCorrente conta2 = (ContaCorrente) _dao.listByObject(conta);
-		
+		_dao.add(contaCorrente);
+
+		ContaCorrente contaCorrente2 = (ContaCorrente) _dao.listByObject(contaCorrente);
+
 		// assert
-		assertEquals(conta.toString(), conta2.toString());
+		assertEquals(contaCorrente.toString(), contaCorrente2.toString());
 	}
-	
+
 	@Test
 	public void add_SecondAccountToSamePerson_MaintainSinglePessoaRecord() {
 		// arrange
@@ -58,68 +62,145 @@ public class ContaCorrenteDAOTests {
 		int numeroConta = 2;
 		double saldo = 250;
 		String situacao = "b";
-		
+
 		Pessoa p = new Pessoa();
 		p.setId(1);
 		p = (Pessoa) new ClienteDAO().listByPrimaryKey(p.getClass(), p.getPrimaryKey());
-		
-		ContaCorrente conta = new ContaCorrente(id, numeroConta, saldo, situacao, p, null);
-		
+
+		ContaCorrente contaCorrente = new ContaCorrente(id, numeroConta, saldo, situacao, p, null);
+
 		// act
-		_dao.add(conta);
-		
-		ContaCorrente conta2 = (ContaCorrente) _dao.listByObject(conta);
+		_dao.add(contaCorrente);
+
+		ContaCorrente contaCorrente2 = (ContaCorrente) _dao.listByObject(contaCorrente);
+
 		// assert
-		assertEquals(conta.toString(), conta2.toString());
+		assertEquals(contaCorrente.toString(), contaCorrente2.toString());
 	}
-	
-	@Ignore
+
 	@Test
 	public void listByObject_ValidArg_ReturnValidRecord() {
 		// arrange
-		
+		int id = 3;
+		int numeroConta = 3;
+		double saldo = 125.0;
+		String situacao = "a";
+
+		Pessoa p = new Pessoa();
+		p.setId(1);
+		p = (Pessoa) new ClienteDAO().listByPrimaryKey(p.getClass(), p.getPrimaryKey());
+
+		ContaCorrente contaCorrente = new ContaCorrente(id, numeroConta, saldo, situacao, p, null);
+
 		// act
-		
+		_dao.add(contaCorrente);
+
+		ContaCorrente contaCorrente2 = (ContaCorrente) _dao.listByObject(contaCorrente);
+
 		// assert
+		assertEquals(contaCorrente.toString(), contaCorrente2.toString());
 	}
 
-	@Ignore
 	@Test
 	public void listByPrimaryKey_ValidArg_ReturnValidRecord() {
 		// arrange
-		
+		int id = 2;
+		ContaCorrente contaCorrente = new ContaCorrente();
+		contaCorrente.setId(id);
+
 		// act
-		
+		ContaCorrente contaCorrente2 = (ContaCorrente) _dao.listByPrimaryKey(contaCorrente.getClass(),
+				contaCorrente.getPrimaryKey());
+
 		// assert
+		assertNotNull(contaCorrente2);
 	}
 
-	@Ignore
 	@Test
 	public void removeByObject_ValidArg_RemoveRecordFromDatabase() {
 		// arrange
-		
+		int id = 4;
+		int numeroConta = 4;
+		double saldo = 140.0;
+		String situacao = "a";
+
+		Pessoa p = new Pessoa();
+		p.setId(1);
+		p = (Pessoa) new ClienteDAO().listByPrimaryKey(p.getClass(), p.getPrimaryKey());
+
+		ContaCorrente contaCorrente = new ContaCorrente(id, numeroConta, saldo, situacao, p, null);
+
 		// act
-		
+		_dao.add(contaCorrente);
+		_dao.removeByObject(contaCorrente);
+
+		contaCorrente = new ContaCorrente();
+		contaCorrente.setId(id);
+		contaCorrente = (ContaCorrente) _dao.listByPrimaryKey(contaCorrente.getClass(), contaCorrente.getPrimaryKey());
+
 		// assert
+		assertNull(contaCorrente);
 	}
 
 	@Ignore
 	@Test
 	public void removeByPrimaryKey_ValidArg_RemoveRecordFromDatabase() {
 		// arrange
-		
+
 		// act
-		
+
 		// assert
 	}
 
-	@Ignore
 	@Test
 	public void update_ValidArg_UpdateRecordInDatabase() {
 		// arrange
-		
+		int id = 5;
+		int numeroConta = 6;
+		double saldo = 245.0;
+		String situacao = "b";
+
+		Pessoa p = new Pessoa();
+		p.setId(1);
+		p = (Pessoa) new ClienteDAO().listByPrimaryKey(p.getClass(), p.getPrimaryKey());
+
+		ContaCorrente contaCorrente = new ContaCorrente(id, numeroConta, saldo, situacao, p, null);
+
 		// act
-		
+		_dao.add(contaCorrente);
+		contaCorrente.setSaldo(500);
+		contaCorrente.setSituacao("a");
+		contaCorrente.setNumeroConta(5);
+		_dao.update(contaCorrente);
+
+		ContaCorrente contaCorrente2 = (ContaCorrente) _dao.listByObject(contaCorrente);
+
 		// assert
+		assertEquals(contaCorrente.toString(), contaCorrente2.toString());
+
 	}
+
+	@Test
+	public void listAll_ValidArg_ReturnAllRecordsFromDatabase() {
+
+		// arrange
+		int id = 6;
+		int numeroConta = 6;
+		double saldo = 85.5;
+		String situacao = "a";
+
+		Pessoa p = new Pessoa();
+		p.setId(1);
+		p = (Pessoa) new ClienteDAO().listByPrimaryKey(p.getClass(), p.getPrimaryKey());
+
+		ContaCorrente contaCorrente = new ContaCorrente(id, numeroConta, saldo, situacao, p, null);
+
+		// act
+		ArrayList<ContaCorrente> list = (ArrayList<ContaCorrente>) _dao.listAllContaCorrente(contaCorrente);
+
+		// assert
+		assertTrue(list.size() == 3);
+
+	}
+
 }
--- src/test/java/dao/ContaPoupancaDAOTests.java
+++ src/test/java/dao/ContaPoupancaDAOTests.java
@@ -1,6 +1,11 @@
 package dao;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -9,96 +14,169 @@ import org.junit.Test;
 
 import dao.conta.ContaPoupancaDAO;
 import dao.pessoa.ClienteDAO;
-
+import model.conta.ContaCorrente;
 import model.conta.ContaPoupanca;
 import model.pessoa.Pessoa;
-import util.UtilJPA;
 
 public class ContaPoupancaDAOTests {
 
 	private ContaPoupancaDAO _dao = new ContaPoupancaDAO();
-	
+
 	@BeforeClass
 	public static void setUp() {
 		System.out.println("Starting Conta Poupança DAO Tests...");
 	}
-	
+
 	@AfterClass
 	public static void tearDown() {
 		System.out.println("Finishing Conta Poupança DAO Tests...");
 //		UtilJPA.closeEntityManagerFactory();
 	}
-	
+
 	@Test
 	public void add_ValidArg_AddToDatabase() {
 		// arrange
-		int id = 3;
-		int numeroConta = 2;
+		int id = 7;
+		int numeroConta = 7;
 		double saldo = 99.0;
 		String situacao = "a";
-		
+
 		Pessoa p = new Pessoa();
 		p.setId(1);
 		p = (Pessoa) new ClienteDAO().listByPrimaryKey(p.getClass(), p.getPrimaryKey());
-		
-		ContaPoupanca conta = new ContaPoupanca(id, numeroConta, saldo, situacao, p, null);
-		
+
+		ContaPoupanca contaPoupanca = new ContaPoupanca(id, numeroConta, saldo, situacao, p, null);
+
 		// act
-		_dao.add(conta);
-		
-		ContaPoupanca conta2 = (ContaPoupanca) _dao.listByObject(conta);
-		
+		_dao.add(contaPoupanca);
+
+		ContaPoupanca contaPoupanca2 = (ContaPoupanca) _dao.listByObject(contaPoupanca);
+
 		// assert
-		assertEquals(conta.toString(), conta2.toString());
+		assertEquals(contaPoupanca.toString(), contaPoupanca2.toString());
 	}
 
-	@Ignore
 	@Test
 	public void listByObject_ValidArg_ReturnValidRecord() {
 		// arrange
-		
+		int id = 8;
+		int numeroConta = 8;
+		double saldo = 325.8;
+		String situacao = "a";
+
+		Pessoa p = new Pessoa();
+		p.setId(1);
+		p = (Pessoa) new ClienteDAO().listByPrimaryKey(p.getClass(), p.getPrimaryKey());
+
+		ContaPoupanca contaPoupanca = new ContaPoupanca(id, numeroConta, saldo, situacao, p, null);
+
 		// act
-		
+		_dao.add(contaPoupanca);
+
+		ContaPoupanca contaPoupanca2 = (ContaPoupanca) _dao.listByObject(contaPoupanca);
+
 		// assert
+		assertEquals(contaPoupanca.toString(), contaPoupanca2.toString());
 	}
 
-	@Ignore
 	@Test
 	public void listByPrimaryKey_ValidArg_ReturnValidRecord() {
 		// arrange
-		
+		int id = 7;
+		ContaPoupanca contaPoupanca = new ContaPoupanca();
+		contaPoupanca.setId(id);
+
 		// act
-		
+		ContaPoupanca contaPoupanca2 = (ContaPoupanca) _dao.listByPrimaryKey(contaPoupanca.getClass(),
+				contaPoupanca.getPrimaryKey());
+
 		// assert
+		assertNotNull(contaPoupanca2);
 	}
 
-	@Ignore
 	@Test
 	public void removeByObject_ValidArg_RemoveRecordFromDatabase() {
 		// arrange
-		
+		int id = 9;
+		int numeroConta = 9;
+		double saldo = 540.0;
+		String situacao = "a";
+
+		Pessoa p = new Pessoa();
+		p.setId(1);
+		p = (Pessoa) new ClienteDAO().listByPrimaryKey(p.getClass(), p.getPrimaryKey());
+
+		ContaPoupanca contaPoupanca = new ContaPoupanca(id, numeroConta, saldo, situacao, p, null);
+
 		// act
-		
+		_dao.add(contaPoupanca);
+		_dao.removeByObject(contaPoupanca);
+
+		contaPoupanca = new ContaPoupanca();
+		contaPoupanca.setId(id);
+		contaPoupanca = (ContaPoupanca) _dao.listByPrimaryKey(contaPoupanca.getClass(), contaPoupanca.getPrimaryKey());
+
 		// assert
+		assertNull(contaPoupanca);
 	}
 
 	@Ignore
 	@Test
 	public void removeByPrimaryKey_ValidArg_RemoveRecordFromDatabase() {
 		// arrange
-		
+
 		// act
-		
+
 		// assert
 	}
 
-	@Ignore
 	@Test
 	public void update_ValidArg_UpdateRecordInDatabase() {
 		// arrange
-		
+		int id = 10;
+		int numeroConta = 33;
+		double saldo = 100.0;
+		String situacao = "b";
+
+		Pessoa p = new Pessoa();
+		p.setId(1);
+		p = (Pessoa) new ClienteDAO().listByPrimaryKey(p.getClass(), p.getPrimaryKey());
+
+		ContaPoupanca contaPoupanca = new ContaPoupanca(id, numeroConta, saldo, situacao, p, null);
+
 		// act
-		
+		_dao.add(contaPoupanca);
+		contaPoupanca.setSaldo(150.5);
+		contaPoupanca.setSituacao("a");
+		contaPoupanca.setNumeroConta(10);
+		_dao.update(contaPoupanca);
+
+		ContaPoupanca contaPoupanca2 = (ContaPoupanca) _dao.listByObject(contaPoupanca);
+
 		// assert
+		assertEquals(contaPoupanca.toString(), contaPoupanca2.toString());
 	}
+
+	@Test
+	public void listAll_ValidArg_ReturnAllRecordsFromDatabase() {
+
+		// arrange
+		int id = 11;
+		int numeroConta = 11;
+		double saldo = 125.5;
+		String situacao = "a";
+
+		Pessoa p = new Pessoa();
+		p.setId(1);
+		p = (Pessoa) new ClienteDAO().listByPrimaryKey(p.getClass(), p.getPrimaryKey());
+
+		ContaPoupanca contaPoupanca = new ContaPoupanca(id, numeroConta, saldo, situacao, p, null);
+
+		// act
+		ArrayList<ContaPoupanca> list = (ArrayList<ContaPoupanca>) _dao.listAllContaPoupanca(contaPoupanca);
+
+		// assert
+		assertTrue(list.size() == 3);
+	}
+
 }
